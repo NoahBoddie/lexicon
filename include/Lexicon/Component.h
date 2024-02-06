5027d6638afa6b77c4dd778c0c95281de31f6419
NoahBoddie <101380447+NoahBoddie@users.noreply.github.com>
2024-02-06 16:28:41 -0500

    Refactored Component
    Just about renamed/reimplemented all functions. New to the set up are OnInit and a reimplemented dependency system. Additionally, made use of some padding to have data for custom flags for derived classes to use.

--- include/Lexicon/Component.h
+++ include/Lexicon/Component.h
@@ -6,36 +6,21 @@ namespace LEX
 {
 	class RoutineArgument;
 	class Component;
-	//Deprecated
-	ENUM(ComponentType_)
+	
+	ENUM(ComponentFlag, uint8_t)
 	{
-		Invalid,		//Type was created outside of factory, and thus not a viable type.
-		Subroutine,
-		Coroutine,
-		GlobalVariable,
+		None = 0,
+		Invalid		= 0b001, //Setting this will fail it to validate, even if it validates later.
+		Valid		= 0b010, //Flag to designate primary validation step achieved
+		Success		= 0b110, //Flag for validation completion, comprised of success for easier comparison.
 
-		Project,
-		Script,
-		CommonScript
-		//Prototype,
-		//Needs the space for the environment bunch.
+		Validation	= 0b111, //All validation flags at once. To get use '&' on flags and get equal to success
+
+		Initialized = 1 << 4, //flag to say that load from view has finished once.
+		Linked		= 1 << 5  //Flag determines that a check for linking occured, not entirely that all links are done.
 	};
 
-	ENUM(ComponentFlags, uint8_t)
-	{
-		Invalid = 0,
-		VailidateFailure = 0b001, //Setting this will fail it to validate, even if it validates later.
-		ValidateSuccess = 0b010, //Flag to designate primary validation step achieved
-		ValidateComplete = 0b110, //Flag for validation completion, comprised of success for easier comparison.
-
-		ValidationFlags = 0b111, //The sum total of all flags used for validation.
-		//To check for validation, you use & on the flags and see if it's equal to ValidateComplete
-
-		ErrorFlag = 1 << 3,//Used when an error is spoken to not print it over and over again. May make more.
-		LoadedFromRecord = 1 << 4, //flag to say that load from view has finished once.
-	};
-
-	ENUM(LinkerFlags, uint8_t)
+	ENUM(LinkFlag, uint8_t)
 	{
 		None,
 		Object		= 1 << 0,	//Used for objects who's reference of may not be visable on the spot.
@@ -44,11 +29,20 @@ namespace LEX
 								// (for AVG, called in postpostload)
 		External	= 1 << 3,	//Used for the reference of properties or formulas and as such must be handled after load.
 		
-		All = LinkerFlags::Object | LinkerFlags::Declaration | LinkerFlags::Definition | LinkerFlags::External,
+		All			= LinkFlag::Object | LinkFlag::Declaration | LinkFlag::Definition | LinkFlag::External,
 
-		Total//What the fuck does this do lmao
+		//Final happens when all linking is done, notably, what also happens here is a bid for dependency.
+		// Basically by now if it's not loaded properly, it will never be. Final also cannot be manually selected.
+		//TODO: So for the above, Component::Link needs a wrapped function.
+		Final		= LinkFlag::All + 1, 
 	};
 	
+	enum struct LinkResult
+	{
+		Success,
+		Failure
+	};
+
 	struct ComponentType
 	{
 		ComponentType() = default;
@@ -75,106 +69,14 @@ namespace LEX
 	};
 
 
-	//IComponent has absolutely no reason to exist further. Slated for deletion
-	struct IComponent
-	{
-		//TODO: Turn off IComponent's RecordConstructible data, and move to main component.
-		virtual void ConstructFromRecord(Record& record){}
-		//Record* GetRecord() override { return nullptr; }
-
-		//TODO:Remove the IComponent::IsValid from virtual, and add a secondary one to determine extra validity.
-		virtual bool IsValid() const { return true; }
-
-		//A lot of delcaration is going to have to be moved here, including load from view
-		// of course, all just declarative. I know this is a mite shit, but it'll have to do.
 
 
-		//This is used to tell if an object is a certain type or not. Useful for delegate that need to know if it's actually storing a subroutine
-		// or if the subroutine was shortened down to it's results, or if it was shortened down to a single call.
-		//virtual ComponentType GetComponentType() const = 0;
-
-		virtual ~IComponent() = default;
-	};
-
-
-	struct Component : public IComponent
+	class Component
 	{
 	private:
-		struct LinkingData//Kinda useless?
-		{
-			LinkerFlags flags{};
-			size_t count{};
-		};
-		//I'd like to use this instead.
-		//std::map<Component*, LinkingData> _linkerContainer{};
-		inline static std::map<Component*, LinkerFlags> _linkerContainer{};
-
-		//To handle link dependency is to handle linking that occurs when things are all complete. I think to that, dependency will fire when _linkCheckFlags
-		// equal LinkerFlags::All. 
-		//A secondary problem is also when a component dies it may not be able to pull itself out of the component library. To that, I think it would be better
-		// to turn this into a vector of pairs, and when something is removed from existing, it will pull it's dependency. I can probably make a function for this, along with
-		// a flag. The idea would basically be that if the left hand doesn't exist, we don't worry about depedency, but if the right hand doesn't exist a dependency has been
-		// compromised.
-		// ^Definitely harder/longer to search but it will only need to be searched once.
-		//Core to the concept of this would be to pluck every the pair every time consideration is going off, and then to make the function recursive, and able
-		// to resolve other components questions.
-		inline static std::map<Component*, std::vector<Component*>> _dependencyContainer{};
-
-		//This is used later on to signify that if All flags have been done, linkage doesn't have to wait.
-		inline static LinkerFlags _linkCheckFlags = LinkerFlags::None;
-
-
-		//inline static std::map<LinkerFlags, std::list<Component*>> linkerMap;
-		//inline static std::map <Component*, uint32_t> linkerCount;
-
-
-		void AttemptCompleteValidation() const;
-
-	protected:
-		//A note on these dependencies and validity, ONLY care about whether it itself is valid, and if it isn't, force
-		// invalidity on things that depend on it.
-		void ClearDependency();
-		void AddDependency(Component* component);
-		void RemoveDependency(Component* component);
-		bool IsDependent(Component* component);
-
-
-		void AttemptValidation() const;
-
-		void AttemptFullValidation() const;
-
-
-
+	
 	public:
-		static void FinalizeLinkage(LinkerFlags link);
-
-
-
-
-
-		//ComponentType GetComponentType() const { return _type; }
- 
-		bool IsValid() const override;
-
-		//Checks if the object had success, regardless if the object itself is still invalid.
-		bool IsPartiallyValid() const;
-
-		//Checks if the object is forcibly invalid due to error.
-		bool IsForcedInvalid() const;
-
-		virtual LinkerFlags GetLinkerFlags() const;
-
-		//We return false because if something sets up linker flags but never sets up the link
-		// handle, it should fail.
-		virtual bool OnLinkage(LinkerFlags link_type);
-
-		void HandleLink(LinkerFlags link_type);
-
-
-		void LogError(std::string log);
-
-		//Just record?
-		//Should take 2 different iterators.
+		
 	public:
 
 
@@ -199,11 +101,7 @@ namespace LEX
 		{
 			D* comp = new D();
 
-			if (rec)
-				comp->ConstructFromRecord(*rec);
-			else {
-				comp->CheckLinkValidation();
-			}
+			comp->Initialize(rec);
 
 			return comp;
 		}
@@ -231,37 +129,358 @@ namespace LEX
 		
 
 		//Contrary to what I stated would be important, without routine items, there's no reason to have this load from data anymore.
-		virtual void LoadFromRecord(Record& rec)
+		virtual void OnInit(Record& rec)
 		{
 			//should likely be a pure virtual, but holding off.
 		}
 
 
-		//virtual void LoadFromRecordData(RGL::RecordDataIterator& data_view){RGL_LOG(info, "{}", data_view->view);}
 
 
 
-		void ConstructFromRecord(Record& rec) override;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		//////////////
+		//new names //
+		//////////////
+		/*
+		Initialize- fires once, create uses, cannot be considered valid what so ever if it's not been fired (to which, it should be known that it's not valid).
+
+		virtual OnLink
+		virtual OnXyzLink (This part is manual)
+
+		PartiallyValid->ValidFlag
+		ForcedInvalid->InvalidFlag
+		IsValid->virtual GetValid
+		IsValid
+
+		FinalizeLinkage->Link
+		CheckLinkValidation ->
+		GetLinkFlag-> GetLinkFlags
+		OnLinkage -> OnLink
+		IsLinked-
+
+
+		AttempteValidation -> FlagAsValid
+		<No function> -> FlagAsInvalid
+		AttemptFullValidation-> delete, whatever used it can just manually check if linking is complete
+		AttemptCompleteValidation-> TryValidate
+
+		//*/
+
+
+
+
+
+		//This name is being taken because the function is used in one place and is small. Better inlined.
+		void HandleLinkage()//private
+		{
+			LinkFlag links = GetLinkFlags();
+
+			if (!!links) {
+				//auto& link_entry = _linkerContainer[this];
+				_linkerContainer[this] = links;
+
+			}
+			else {
+				TryValidate();
+			}
+		}
+
+		void Initialize(Record* rec) 
+		{
+			if (IsInitialized() == false)
+			{
+				if (rec)
+					OnInit(*rec);
+
+				_flags |= ComponentFlag::Initialized;
+
+				HandleLinkage();
+			}
+			
+		}
+		
+		void Initialize(Record& rec)
+		{
+			return Initialize(&rec);
+		}
+
+		bool IsInitialized() const
+		{
+			return _flags & ComponentFlag::Initialized;
+		}
+
+
+		//With no record, on init shouldn't be used.
+		//virtual void OnInit(Record& rec)
+		static void Link(LinkFlag flags) 
+		{
+			//Make sure to remove the linkCheckFlags
+
+			auto end = _linkerContainer.rend();
+
+			for (auto it = _linkerContainer.rbegin(); it != end;)
+			{
+				LinkFlag& tasks = it->second;
+
+				LinkFlag flag = flags & tasks;
+
+				Component* target = it->first;
+
+				bool can_validate = true;
+
+				if (!flag)
+					goto _continue;
+
+
+				//Its also possible the impl version of the call can do this for me.
+				if (target->OnLink(flag) == LinkResult::Success) {
+					target->FlagAsValid();
+				}
+				else {
+					target->FlagAsInvalid();
+				}
+
+
+				//If the validation has failed, it will cease to attempt to validate it.
+				can_validate = target->InvalidFlag();
+
+				if ((tasks &= ~flag) && !can_validate)
+				{
+				_continue:
+					it++;
+				}
+				else
+				{
+					auto del = it++;
+					if (can_validate)
+						target->TryValidate();
+
+					_linkerContainer.erase(del->first);
+				}
+			}
+
+			_linkCheckFlags |= flags;
+
+			RGL_LOG(trace, "Finalized linkage: {}", (int)flags);
+
+			//Should it have processed everything it should remove it all.
+		}
+
+		virtual LinkResult OnLink(LinkFlag flags) { return LinkResult::Failure; }
+
+		virtual LinkFlag GetLinkFlags() { return LinkFlag::None; }
+
+		bool ValidFlag() const
+		{
+			return (_flags & ComponentFlag::Valid);
+		}
+
+		bool InvalidFlag() const
+		{
+			return (_flags & ComponentFlag::Valid);
+		}
+
+		
+
+		//more protected. By default, this is a question about the options of the derived class.
+		virtual bool GetValid() const { return true; }
+		
+		bool IsValid() const
+		{
+			return (_flags & ComponentFlag::Validation) == ComponentFlag::Success && GetValid();
+		}
+
+		//Checks flag and also the linker cache
+		bool IsLinked() const
+		{
+			return _flags & ComponentFlag::Linked && _linkerContainer.contains(const_cast<Component*>(this));
+		}
+
+		void FlagAsValid() const
+		{
+			_flags |= ComponentFlag::Valid;
+		}
+		void FlagAsInvalid() const
+		{
+			_flags |= ComponentFlag::Invalid;
+		}
+
+		bool TryValidate()
+		{
+			if (InvalidFlag() == true || ValidFlag() == false)
+				return false;
+
+			_flags |= ComponentFlag::Success;
+			
+			return true;
+		}
 
 
 	private:
-		void CheckLinkValidation();
+	
 
 	public:
-		//Feel like I wanna move this. Should likely befriend its factory instead.
-		// I should mirror the private constructed class singleton cobb made instead.
 		Component() = default;
 		Component(const Component&) = delete;
 		Component(const Component&&) = delete;
 		Component& operator= (Component) = delete;
 		Component& operator= (const Component&) = delete;
 		Component& operator= (const Component&&) = delete;
-		virtual ~Component() { _linkerContainer.erase(this); }
+		virtual ~Component() { _linkerContainer.erase(this); AbsolveDependency(); }
 
 	private:
-		//ComponentType _type = ComponentType::Invalid;
-		mutable ComponentFlags _flags = ComponentFlags::Invalid;
+		inline static std::map<Component*, LinkFlag> _linkerContainer{};
 
+		//To handle link dependency is to handle linking that occurs when things are all complete. I think to that, dependency will fire when _linkCheckFlags
+		// equal LinkFlag::All. 
+		//A secondary problem is also when a component dies it may not be able to pull itself out of the component library. To that, I think it would be better
+		// to turn this into a vector of pairs, and when something is removed from existing, it will pull it's dependency. I can probably make a function for this, along with
+		// a flag. The idea would basically be that if the left hand doesn't exist, we don't worry about depedency, but if the right hand doesn't exist a dependency has been
+		// compromised.
+		// ^Definitely harder/longer to search but it will only need to be searched once.
+		//Core to the concept of this would be to pluck every the pair every time consideration is going off, and then to make the function recursive, and able
+		// to resolve other components questions.
+		inline static std::map<Component*, std::vector<size_t>> _dependencyMap{};
+
+		inline static std::vector<Component*> _dependeeList{};
+public:
+		void AbsolveDependency() const
+		{
+			_dependencyMap.erase(const_cast<Component*>(this));
+			
+			if (auto it = std::find(_dependeeList.begin(), _dependeeList.end(), this); _dependeeList.end() != it)
+				*it = nullptr;
+		}
+
+		static void ClearDependencies() 
+		{
+			_dependencyMap.clear();
+			_dependeeList.clear();
+			_dependeeList.shrink_to_fit();
+
+		}
+
+		size_t GetDependee() const
+		{
+			if (this)
+			{
+				size_t size = _dependeeList.size();
+
+				for (size_t i = 0; i < size; i++)
+				{
+					if (_dependeeList[i] == this)
+					{
+						return i;
+					}
+				}
+			}
+
+			return max_value<size_t>;
+		}
+
+		size_t ObtainDependee()
+		{
+			size_t dep = GetDependee();
+
+			if (dep == max_value<size_t>)
+			{
+				dep = _dependeeList.size();
+				_dependeeList.push_back(this);
+			}
+
+			return dep;
+		}
+
+		void AddDependency(Component* component)
+		{
+			if (component)
+			{
+				size_t index = component->ObtainDependee();
+
+				if (auto it = _dependencyMap.find(this); _dependencyMap.end() != it)
+				{
+					auto& list = it->second;
+
+					if (auto res = std::find(list.begin(), list.end(), index); list.end() != res) {
+						list.push_back(index);
+					}
+				}
+			}
+		}
+
+		void RemoveDependency(Component* component)
+		{
+			size_t index = component->GetDependee();
+
+			if (auto it = _dependencyMap.find(this); _dependencyMap.end() != it)
+			{
+				auto& list = it->second;
+
+				auto res = std::find(list.begin(), list.end(), index);
+
+				list.erase(res);
+			}
+		}
+
+
+		bool IsDependeeOf(Component* component) const
+		{
+			component->IsDependentTo(this);
+		}
+
+		bool IsDependentTo(const Component* component) const
+		{
+			if (component)
+			{
+				size_t index = component->GetDependee();
+
+				if (index != max_value<size_t>)
+				{
+					if (auto it = _dependencyMap.find(const_cast<Component*>(this)); _dependencyMap.end() != it)
+					{
+						auto& list = it->second;
+
+						auto res = std::find(list.begin(), list.end(), index);
+
+						return list.end() != res;
+					}
+				}
+			}
+
+			return false;
+		}
+
+		template <typename T>
+		T& GetComponentData()
+		{
+			return reinterpret_cast<T&>(_data);
+		}
+
+		private:
+
+		//This is used later on to signify that if All flags have been done, linkage doesn't have to wait.
+		inline static LinkFlag _linkCheckFlags = LinkFlag::None;
+
+
+		//ComponentType _type = ComponentType::Invalid;
+		mutable ComponentFlag _flags = ComponentFlag::Invalid;
+
+		//Data usable by any person to store personal data here. After all, it's free space.
+		mutable uint32_t _data = 0;
 	};
 
 	//This has 2 more derived classes, one that gets the generic record it derives from, and then another that gets the beginning and end
