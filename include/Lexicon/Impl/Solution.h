148dd71782f83e42a7de4fb14b47a385aea9fdd1
NoahBoddie <101380447+NoahBoddie@users.noreply.github.com>
2024-02-07 12:53:34 -0500

    Added qualifiers and assignment operator
    -Allowed the Operand part of the Solution to be edited via the another Operand without touching the Solution bits.
    -Added qualifiers to help determine how to view the data being given.

--- include/Lexicon/Impl/Solution.h
+++ include/Lexicon/Impl/Solution.h
@@ -4,24 +4,19 @@
 #include "ITypePolicy.h"
 #include "OperandType.h"
 #include "Operand.h"
+
+#include "BasicQualifier.h"
+#include "RuntimeQualifier.h"
+
 namespace LEX
 {
 	struct Solution : public Operand
 	{
 		ITypePolicy* policy{};		//I think I use an ITypePolicy? That's sorta the rule here.
-		//Operand result{};
-		
-		//Target value;//holds the accessing value, index, pointer, whatever.
-		
-		//OperandType type;//denotes what the return type should be.
-		//Should come with additional flags and values.
-		// Like, is it an lhs, or rhs that's expected to be pushed? Of which, it has to be one. Just one, not dynamic.
-		// this can control what it believes can occur to it.
 
-		//OR, is the expected value a constant? All very important questions.
+		RuntimeQualifier runtimeFlags{};
+		BasicQualifier basicFlags{};
 
-		//Need some way to convey if this is a ref or not. Might be if it's using registers or not, 
-		// but might have some flags.
 
 
 		//Solution needs some qualifiers, but to that it needs 3 types
@@ -54,5 +49,19 @@ namespace LEX
 		{
 			//RGL_LOG(trace, "sol ctor ops {}", a_type);
 		}
+
+
+		constexpr Solution& operator= (Operand& rhs) noexcept
+		{
+			Operand::operator=(rhs);
+			return *this;
+		}
+
+		constexpr Solution& operator= (Operand&& rhs) noexcept
+		{
+			return Solution::operator=(rhs);
+		}
+
+		
 	};
 }
