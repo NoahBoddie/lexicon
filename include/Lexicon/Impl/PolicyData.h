b4a51773cd5c934d47fc3ca5e53e8804a98abd9d
NoahBoddie <101380447+NoahBoddie@users.noreply.github.com>
2024-04-29 22:16:05 -0400

    Added ObjectPolicy, category, and offset to PolicyData

--- include/Lexicon/Impl/PolicyData.h
+++ include/Lexicon/Impl/PolicyData.h
@@ -1,9 +1,15 @@
 #pragma once
 
+#include "TypeID.h"
+#include "DataType.h"
 
 namespace LEX
 {
+	struct ITypePolicy;
+
 	class ConcretePolicy;
+
+	FRWD_DECL_OBJECT_POLICY;
 
 	//Give their own.
 
@@ -20,18 +26,12 @@ namespace LEX
 		Signature,
 	};
 
-	enum struct DataType
-	{
-		Invalid,
-		Class,
-		Struct,
-		Interface,
-	};
-
 
 	struct PolicyData
 	{
 	public:
+		//Inheritance will look more like a vector of inheritance data, telling the hash from the main type, it's distance, etc etc.
+
 
 		//Is an ITypePolicy for generic classes. Concrete can just submit without error, while Special needs to submit types (or it will just store it's specialized one)
 		ITypePolicy* _extends;//Deprecated
@@ -45,7 +45,14 @@ namespace LEX
 
 		String _name;
 
+
 		DataType _dataType = DataType::Invalid;
+
+		//Should be inherited, only one can exist.
+		OBJECT_POLICY* policy = nullptr;
+
+		std::string category;
+		TypeOffset offset;
 
 		//AbstractTypePolicy* extends() { return _extends; }
 	};
