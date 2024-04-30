29e85f98cc5a0176a110180e7ba7f1d99885baa9
NoahBoddie <101380447+NoahBoddie@users.noreply.github.com>
2024-04-29 21:52:13 -0400

    Refactored IdentityManager, made it an Interface

--- include/Lexicon/Impl/IdentityManager.cpp
+++ include/Lexicon/Impl/IdentityManager.cpp
@@ -1,153 +1,144 @@
-/*
-
+#pragma once
 
 #include "IdentityManager.h"
 
-//src
-#include "ITypePolicy.h"
 
+//src
+#include "PolicyBase.h"
+#include "ITypePolicy.h"
+#include "VoidPolicy.h"
 namespace LEX
 {
 
-	IdentitySet* IdentityManager::_GetIDSet(uint32_t id)
+	
+	struct IdentityData
 	{
-		if (id < nextID)
-		{
-			auto size = idList.size();
+		std::string_view name;
 
-			for (int i = 0; i <= size; i++)
-			{
-				if (i == size || idList[i].startValue > id)
-					return &idList[i - 1];
-			}
+		uint32_t startID = 0;
+		uint32_t range = 0;
+
+
+	public:
+		TypeID GetTypeID(uint16_t offset)
+		{
+			assert(offset < range);
+
+			if (offset < range)
+				return TypeID::invalidValue;
+
+			return startID + offset;
 		}
 
-
-		return nullptr;
-	}
-	TypeCode IdentityManager::_GetTypeCode(uint32_t id)
-	{
-		if (id < nextID)
+		constexpr IdentityData(std::string_view n, uint32_t val, uint16_t rng) :
+			name{ n }, startID{ val }, range{ rng }
 		{
-			auto size = idList.size();
-
-			for (int i = 0; i <= size; i++)
-			{
-				if (i == size || idList[i].startValue > id) {
-					TypeIndex index = i - 1;
-					TypeOffset offset = static_cast<TypeOffset>(id - idList[i - 1].startValue);
-					RGL_LOG(trace, "{} code offset vs {}", offset, id);
-
-					return TypeCode::Create(index, offset);
-				}
-
-			}
 		}
 
+	};
+	
+	struct PolicyEntry
+	{
+		PolicyBase* policy = nullptr;
+		uint32_t index = 0;//Index serves as easy access to a category.
 
-		return {};
+	};
+
+
+
+
+
+
+	inline std::mutex _lock;
+	inline std::vector<IdentityData> dataList;
+	//Policy list starts with an entry immediately. The void policy.
+	inline std::vector<PolicyBase*> policyList{ nullptr };
+
+	inline PolicyBase* _voidPolicy = new VoidPolicy{};
+
+	//Not needed now policy list exists.
+	inline uint32_t nextID = 1;
+
+
+
+
+	uint32_t IdentityManager::GetIDFromIndex(TypeIndex index)
+	{
+		assert(dataList.size() > index);
+
+		return dataList[index].startID;
+	}
+
+	TypeIndex IdentityManager::GetIndexFromName(std::string_view name)
+	{
+		auto size = dataList.size();
+
+		for (int i = 0; i < size; i++)
+		{
+			auto& entry_name = dataList[i].name;
+
+			if (!entry_name.empty() && entry_name == name)
+				return i;
+		}
+
+		return -1;
 	}
 
 
-
-	TypeID IdentityManager::ClaimID(ITypePolicy* policy, TypeIndex index, TypeOffset offset)
+	//This feels like it's really only going to be used for the other 2.
+	TypeID IdentityManager::ClaimID(PolicyBase* policy, uint32_t id)
 	{
-		//Make this one that uses type code.
+		//Fix.
+		std::lock_guard<std::mutex> guard{ _lock };
 
-		//This gets the id set, and then uses the offset
+		assert(policyList.size() > id);
 
-		//I can't use abstract only, because generic stuff shouldn't have those.
 
-		ITypePolicy*& slot = idList[index].GetAtOffset(offset);
+		PolicyBase*& slot = policyList[id];
+
+		if (slot) {
+			report::compile::fatal("Slot for {} already taken.", id);
+		}
 
 		//check here 
 
 		slot = policy;
-		RGL_LOG(debug, "offset in claim id {}", offset);
-		TypeID id = TypeID::CreateID(idList[index].startValue + offset);
 
-		logger::info("is??? {}", !policy);
+		TypeID ret = TypeID::CreateID(id);
 
 		//assign policy id here.
-		if (policy)
-			policy->SetTypeID(id);
+		policy->SetTypeID(ret);
 
-		return id;
+		return ret;
 	}
 
-	TypeID IdentityManager::ClaimID(ITypePolicy* policy, std::string_view name, TypeOffset offset)
+
+
+	//should return the index.
+	uint32_t IdentityManager::GenerateID(std::string_view name, uint16_t range)
 	{
-		//This just uses the raw value. Will use the above "ClaimID"
+		//Generates a grouped ID instead of a single one.
+		std::lock_guard<std::mutex> guard{ _lock };
 
-		//Would like to make this better later.
-		uint32_t index = GetIndexFromName(name);
-		return ClaimID(policy, index, offset);
+		uint32_t nextID = policyList.size();
 
+		TypeIndex index = dataList.size();
+
+		auto size = policyList.size();
+
+		dataList.emplace_back(name, nextID, ++range);
+
+
+
+		policyList.resize(size + range);
+		//post resize, this should probably fill each entry with an index.
+
+		return index;
 	}
 
-	TypeID IdentityManager::ClaimID(ITypePolicy* policy, uint32_t id)
+	uint32_t IdentityManager::ObtainID(PolicyBase* policy)
 	{
-		//This just uses the raw value. Will use the above "ClaimID"
-
-		//Would like to make this better later.
-		uint32_t index = GetOffsetFromID(id);
-		uint32_t offset = id - idList[index].startValue;
-		return ClaimID(policy, index, offset);
-
-	}
-
-	//This is for when one needs to manually specify each entry, thus it will not be claiming ids
-	uint32_t IdentityManager::GenerateID(std::string_view debug_name, uint16_t range)
-	{
-		//Generate a type code instead so I can make it a type ID.
-
-
-		//thread locking to prevent 2 things from getting the same ID.
-
-		std::mutex lock;
-
-		std::lock_guard<std::mutex> guard{ lock };
-
-		uint32_t id = nextID;
-
-		size_t size = idList.size();
-
-		idList.emplace_back(debug_name, nextID, ++range);
-		nextID += range;
-
-		return id;
-	}
-
-	//This does the deed automatically, should link to each possible itype that inherits this one.
-	//Due to the fact it also claims as it generates, a different name may be in order.
-	uint32_t IdentityManager::ObtainID(FieldDirectory script_dir, uint16_t range)
-	{
-		//DEPRECATED, I don't think I want to make this for 
-
-
-		//This isn't used on interfaces, who largely know who and where they go to, this is used on class types.
-
-		//thread locking to prevent 2 things from getting the same ID.
-
-		std::mutex lock;
-
-		std::lock_guard<std::mutex> guard{ lock };
-
-		uint32_t id = nextID;
-
-		size_t size = idList.size();
-
-		idList.emplace_back(script_dir.GetFullName(), nextID, ++range);
-		nextID += range;
-
-		return id;
-	}
-
-	uint32_t IdentityManager::ObtainID(ITypePolicy* policy)//, uint16_t range = 0)
-	{
-		//This isn't used on interfaces, who largely know who and where they go to, this is used on class types.
-
-		//thread locking to prevent 2 things from getting the same ID.
+		uint32_t nextID = policyList.size();
 
 		if (!policy) {
 			return 0;
@@ -156,129 +147,37 @@ namespace LEX
 			return id_query;
 		}
 
-		std::mutex lock;
-
-		std::lock_guard<std::mutex> guard{ lock };
+		std::lock_guard<std::mutex> guard{ _lock };
 
 		TypeID id = TypeID::CreateID(nextID);
 
-		size_t index = idList.size();
 
-		idList.emplace_back("@Loose", nextID, 0);
-
-		RGL_LOG(debug, "offset in obtain id {}", nextID);
-
-		//New stuff
-		ITypePolicy*& slot = idList[index].GetAtOffset(0);
-
-		//check here 
-
-		slot = policy;
-		//-end new stuff here
+		dataList.emplace_back("", nextID, 1);//shouldn't this be 1 since it's a size?
+		policyList.emplace_back(policy);
 
 		//assign policy id here.
 		policy->SetTypeID(id);
-
-		nextID++;
 
 		return id;
 	}
 
 
-	uint32_t IdentityManager::GetIndexFromName(std::string_view name)
-	{
-		auto size = idList.size();
 
-		for (uint32_t i = 0; i < size; i++)
-		{
-			if (idList[i].name == name)
-				return i;
-		}
-
-		return max_value<uint32_t>;
-	}
-
-
-
-	uint32_t IdentityManager::GetIndexFromID(uint32_t id)
-	{
-		return _GetTypeCode(id).offset;
-
-		if (id < nextID)
-		{
-			auto size = idList.size();
-
-			for (int i = 0; i < size; i++)
-			{
-				if (idList[i].startValue > id)
-					return i - 1;
-			}
-		}
-
-
-		return max_value<uint32_t>;
-	}
-
-	uint32_t IdentityManager::GetOffset(uint32_t id)
-	{//this is not right
-		return _GetTypeCode(id).offset;
-
-		//This will be useful to move elsewhere.
-		if (id < nextID)
-		{
-			auto size = idList.size();
-
-			for (int i = 0; i < size; i++)
-			{
-				if (idList[i].startValue > id)
-					return i - 1;
-			}
-		}
-
-
-		return max_value<uint32_t>;
-	}
-
-	uint16_t IdentityManager::GetOffsetFromID(TypeID id)
-	{
-		return GetOffset(id);
-	}
-
-
-
-	TypeID IdentityManager::GetIDFromOffset(uint32_t index, uint16_t offset)
-	{
-		//TODO: GetIDFromOffset needs checks
-
-		return idList[index].startValue + offset;
-	}
-
-	TypeID IdentityManager::GetIDFromOffset(std::string_view name, uint16_t offset)
-	{
-		return GetIDFromOffset(GetIndexFromName(name), offset);
-	}
-
-	ITypePolicy* IdentityManager::GetTypeByOffset(std::string_view name, TypeOffset offset)
-	{
-		//Change this things name so I can include everything in core later.
-
-		TypeCode code = TypeCode::Create(GetIndexFromName(name), offset);
-		RGL_LOG(trace, "@offset {}:{}", code.index, code.offset);
-
-		//I don't care about safies right now
-
-		return idList[code.index].GetAtOffset(code.offset);
-	}
-
+	
 	ITypePolicy* IdentityManager::GetTypeByID(TypeID id)
 	{
-		TypeCode code = _GetTypeCode(id);
-		RGL_LOG(trace, "@id {}:{}", code.index, code.offset);
-		//I don't care about safies right now
+		return GetBaseByID(id);
+	}
+	//This too is used often.(What does this mean?)
+	PolicyBase* IdentityManager::GetBaseByID(TypeID id)
+	{
+		//TODO: The first type should be the void policy, it should already be generated.
+		if (id == 0) {
+			logger::info("void {}", !!_voidPolicy);
+			return _voidPolicy;
+		}
 
-		return idList[code.index].GetAtOffset(code.offset);
+		return policyList[id];
 	}
 
 }
-
-//*/
--- include/Lexicon/Impl/IdentityManager.h
+++ include/Lexicon/Impl/IdentityManager.h
@@ -3,348 +3,54 @@
 //cut
 #include "TypeID.h"
 
+#include "Interface.h"
+#include "InterfaceSingleton.h"
 
-//src
-#include "PolicyBase.h"
-#include "ITypePolicy.h"
+#include "Versioning.h"
+
 
 namespace LEX
 {
 	struct ITypePolicy;
+	struct PolicyBase;
 
-	
 
-	struct IdentitySet
+	namespace Version
 	{
-		//This is the object that handles the management of the ranges of objects
+		namespace _1
+		{
+			struct INTERFACE_VERSION(IdentityManager)
+			{
+				virtual ITypePolicy* GetTypeByID(TypeID id) = 0;
+				virtual uint32_t GetIDFromIndex(TypeIndex index) = 0;
+				virtual TypeIndex GetIndexFromName(std::string_view name) = 0;
+			};
+		}
 
-		//Might be the name that is given to the set, like core types or SkyrimObjects. Mainly used as a
-		// debug thing.
-		std::string_view name;
+		CURRENT_VERSION(IdentityManager, 1);
+	}
 
-		//This is the name of the 
-		uint32_t startValue = 0;
-		uint16_t offSize = 0;
+
+	struct IMPL_VERSION(IdentityManager)
+	{
+		ITypePolicy* GetTypeByID(TypeID id) INTERFACE_METHOD;//Isn't this supposed to be public?
+		uint32_t GetIDFromIndex(TypeIndex index) INTERFACE_METHOD;
+		TypeIndex GetIndexFromName(std::string_view name) INTERFACE_METHOD;
 		
+		uint32_t GetIDFromName(std::string_view name) { return GetIDFromIndex(GetIndexFromName(name)); }
+		ITypePolicy* GetTypeByOffset(std::string_view name, TypeOffset offset) { return GetTypeByID(GetIDFromName(name) + offset); }
+		
+	INTERNAL://These do not have any formal implementation outside of the source, and as such are privated and invalid outside of it.
+		
+		PolicyBase* GetBaseByID(TypeID id) INTERFACE_FUNCTION;
 
-	private:
-		//Set and truncated to the offset value.
-		//I'm thinking the range must be 16 bit.
-		std::vector<PolicyBase*> _range{};
+		uint32_t ObtainID(PolicyBase * policy) INTERFACE_FUNCTION;
+		uint32_t GenerateID(std::string_view name, uint16_t range) INTERFACE_FUNCTION;
+		TypeID ClaimID(PolicyBase* policy, uint32_t id) INTERFACE_FUNCTION;
 
-	public:
-		TypeID GetTypeID(uint16_t offset)
-		{
-			return startValue + offset;
-		}
 
-		PolicyBase*& GetAtOffset(uint16_t offset)
-		{
-			assert(offset < offSize);
-			
-			if (auto size = _range.size(); size < offset && offSize > size){
-				_range.resize(min(offset + 1, offSize));
-			}
-			//RGL_LOG(trace, " size end:{}", _range.size());
-			return _range[offset];
-		}
-
-		IdentitySet(std::string_view n, uint32_t val, uint16_t rng) :
-			name{ n }, startValue{ val }, offSize{ rng }
-		{
-			//IDEA, if the name is empty, it's determined to be a vacant lot basically. 
-			// I'll want to use this for 
-		}
-
+		PolicyBase* GetBaseByOffset(std::string_view name, TypeOffset offset) { return GetBaseByID(GetIDFromName(name) + offset); }
+		TypeID ClaimID(PolicyBase* policy, TypeIndex index, TypeOffset offset) { return ClaimID(policy, GetIDFromIndex(index) + offset); }
+		TypeID ClaimID(PolicyBase* policy, std::string_view name, TypeOffset offset) { return ClaimID(policy, GetIndexFromName(name), offset); }
 	};
-
-	//The class that manages the base TypeID system for all objects, classes
-	class IdentityManager
-	{
-		inline static std::vector<IdentitySet> idList;//This ID list should start with the null type.
-
-		//Zero is a claimed id, I'll make it likely an actual value.
-		inline static uint32_t nextID = 1;
-
-	public:
-		//TODO: Unpublic this, and make this shit deal with this itself.
-		inline static PolicyBase* _voidPolicy = nullptr;
-
-	protected:
-		static IdentitySet* _GetIDSet(uint32_t id)
-		{
-			if (id < nextID)
-			{
-				auto size = idList.size();
-
-				for (int i = 0; i <= size; i++)
-				{
-					if (i == size || idList[i].startValue > id)
-						return &idList[i - 1];
-				}
-			}
-
-
-			return nullptr;
-		}
-		static TypeCode _GetTypeCode(uint32_t id)
-		{
-			if (id < nextID)
-			{
-				auto size = idList.size();
-
-				for (int i = 0; i <= size; i++)
-				{
-					if (i == size || idList[i].startValue > id) {
-						TypeIndex index = i - 1;
-						TypeOffset offset = static_cast<TypeOffset>(id - idList[i - 1].startValue);
-						RGL_LOG(trace, "{} code offset vs {}", offset, id);
-
-						return TypeCode::Create(index, offset);
-					}
-						
-				}
-			}
-
-
-			return {};
-		}
-	public:
-
-
-		static TypeID ClaimID(PolicyBase* policy, TypeIndex index, TypeOffset offset)
-		{
-			//Make this one that uses type code.
-			
-			//This gets the id set, and then uses the offset
-
-			//I can't use abstract only, because generic stuff shouldn't have those.
-			
-			PolicyBase*& slot = idList[index].GetAtOffset(offset);
-			
-			//check here 
-			
-			slot = policy;
-			RGL_LOG(debug, "offset in claim id {}", offset);
-			TypeID id = TypeID::CreateID(idList[index].startValue + offset);
-
-			logger::info("is??? {}", !policy);
-
-			//assign policy id here.
-			policy->SetTypeID(id);
-			
-			return id;
-		}
-
-		static TypeID ClaimID(PolicyBase* policy, std::string_view name, TypeOffset offset)
-		{
-			//This just uses the raw value. Will use the above "ClaimID"
-
-			//Would like to make this better later.
-			uint32_t index = GetIndexFromName(name);
-			return ClaimID(policy, index, offset);
-
-		}
-
-		static TypeID ClaimID(PolicyBase* policy, uint32_t id)
-		{
-			//This just uses the raw value. Will use the above "ClaimID"
-
-			//Would like to make this better later.
-			uint32_t index = GetOffsetFromID(id);
-			uint32_t offset = id - idList[index].startValue;
-			return ClaimID(policy, index, offset);
-
-		}
-
-		//This is for when one needs to manually specify each entry, thus it will not be claiming ids
-		static uint32_t GenerateID(std::string_view debug_name = "", uint16_t range = 0)
-		{
-			//Generate a type code instead so I can make it a type ID.
-
-
-			//thread locking to prevent 2 things from getting the same ID.
-
-			static std::mutex lock;
-
-			std::lock_guard<std::mutex> guard{ lock };
-
-			uint32_t id = nextID;
-
-			size_t size = idList.size();
-
-			idList.emplace_back(debug_name, nextID, ++range);
-			nextID += range;
-
-			return id;
-		}
-
-		//This does the deed automatically, should link to each possible itype that inherits this one.
-		//Due to the fact it also claims as it generates, a different name may be in order.
-		static uint32_t ObtainID(std::string script_dir, uint16_t range = 0)
-		{
-			//DEPRECATED, I don't think I want to make this for 
-
-
-			//This isn't used on interfaces, who largely know who and where they go to, this is used on class types.
-
-			//thread locking to prevent 2 things from getting the same ID.
-
-			static std::mutex lock;
-
-			std::lock_guard<std::mutex> guard{ lock };
-
-			uint32_t id = nextID;
-
-			size_t size = idList.size();
-			//TODO: Fix up ObtainID. its out of sorts.
-			//idList.emplace_back(script_dir.GetFullName(), nextID, ++range);
-			nextID += range;
-
-			return id;
-		}
-
-		static uint32_t ObtainID(PolicyBase* policy)//, uint16_t range = 0)
-		{
-			//This isn't used on interfaces, who largely know who and where they go to, this is used on class types.
-
-			//thread locking to prevent 2 things from getting the same ID.
-
-			if (!policy) {
-				return 0;
-			}
-			else if (auto id_query = policy->GetTypeID(); id_query != 0) {
-				return id_query;
-			}
-
-			static std::mutex lock;
-
-			std::lock_guard<std::mutex> guard{ lock };
-
-			TypeID id = TypeID::CreateID(nextID);
-
-			size_t index = idList.size();
-
-			idList.emplace_back("@Loose", nextID, 0);
-
-			RGL_LOG(debug, "offset in obtain id {}", nextID);
-
-			//New stuff
-			PolicyBase*& slot = idList[index].GetAtOffset(0);
-
-			//check here 
-
-			slot = policy;
-			//-end new stuff here
-			
-			//assign policy id here.
-			policy->SetTypeID(id);
-
-			nextID++;
-
-			return id;
-		}
-
-
-		static uint32_t GetIndexFromName(std::string_view name)
-		{
-			auto size = idList.size();
-
-			for (uint32_t i = 0; i < size; i++)
-			{
-				if (idList[i].name == name)
-					return i;
-			}
-
-			return max_value<uint32_t>;
-		}
-
-
-
-		static uint32_t GetIndexFromID(uint32_t id)
-		{
-			return _GetTypeCode(id).offset;
-
-			if (id < nextID)
-			{
-				auto size = idList.size();
-
-				for (int i = 0; i < size; i++)
-				{
-					if (idList[i].startValue > id)
-						return i - 1;
-				}
-			}
-
-
-			return max_value<uint32_t>;
-		}
-
-		static uint32_t GetOffset(uint32_t id)
-		{//this is not right
-			return _GetTypeCode(id).offset;
-			
-			//This will be useful to move elsewhere.
-			if (id < nextID)
-			{
-				auto size = idList.size();
-
-				for (int i = 0; i < size; i++)
-				{
-					if (idList[i].startValue > id)
-						return i - 1;
-				}
-			}
-
-
-			return max_value<uint32_t>;
-		}
-
-		static uint16_t GetOffsetFromID(TypeID id)
-		{
-			return GetOffset(id);
-		}
-
-
-
-		static TypeID GetIDFromOffset(uint32_t index, uint16_t offset)
-		{
-			//TODO: GetIDFromOffset needs checks
-
-			return idList[index].startValue + offset;
-		}
-
-		static TypeID GetIDFromOffset(std::string_view name, uint16_t offset)
-		{
-			GetIDFromOffset(GetIndexFromName(name), offset);
-		}
-
-		static PolicyBase* GetTypeByOffset(std::string_view name, TypeOffset offset)
-		{
-			//Change this things name so I can include everything in core later.
-
-			TypeCode code = TypeCode::Create(GetIndexFromName(name), offset);
-			RGL_LOG(trace, "@offset {}:{}", code.index, code.offset);
-
-			//TODO: I don't care about safies right now  but this will cause issues.
-
-			return idList[code.index].GetAtOffset(code.offset);
-		}
-
-		static PolicyBase* GetTypeByID(TypeID id)
-		{
-			if (!id) {
-				return _voidPolicy;
-			}
-
-			TypeCode code = _GetTypeCode(id);
-			RGL_LOG(trace, "@id {}:{}", code.index, code.offset);
-			//I don't care about safies right now
-			
-			return idList[code.index].GetAtOffset(code.offset);
-		}
-
-		//TODO: The next concept for IdenMan is figuring out how to make use of the given range
-	};
-
 }
