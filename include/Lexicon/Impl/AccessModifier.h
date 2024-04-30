0d04d7a646d145129006cd3ba1c3d9af482a0f67
NoahBoddie <101380447+NoahBoddie@users.noreply.github.com>
2024-04-29 21:32:04 -0400

    AccessModifier deprecated
    Access modifiers are moved to DeclareSpecifiers

--- include/Lexicon/Impl/AccessModifier.h
+++ include/Lexicon/Impl/AccessModifier.h
@@ -5,16 +5,16 @@ namespace LEX
 {
 	//Is access specifier.
 
-	enum struct AccessModifier
+	enum struct AccessModifier : uint8_t
 	{//The base struct that handles this might be later named "ClassicInfos". Maybe. Will decide if it sounds dumb.
 
 		Private,	//Only a given scope can observe it
 		Protected,	//Only the scope and derived scopes can observe it (derived data types or required/imported/included scripts). Means different things between data type and 
-		Internal,	//Only the script can observe it.
+		
 		//ProtectedInternal,//extends internal to derived scripts. In a class functions much like internal.
-		Public
+		Public,
 
-
+		Internal = 1 << 7,	//Only the script can observe it. Is a flag rather than another thing
 		//Protected		Only the scope and derived scopes can observe it (derived data types or required/imported/included scripts). Means different things between data type and 
 		//				script block usage.
 	};
