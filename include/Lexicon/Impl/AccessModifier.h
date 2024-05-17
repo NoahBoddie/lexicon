b605c817616fb49f8aa41112229cb52fa0394365
NoahBoddie <101380447+NoahBoddie@users.noreply.github.com>
2024-05-17 08:17:58 -0400

    AccessModifier renamed to Access
    Added shorthands for internal accesses

--- include/Lexicon/Impl/AccessModifier.h
+++ include/Lexicon/Impl/AccessModifier.h
@@ -5,17 +5,25 @@ namespace LEX
 {
 	//Is access specifier.
 
-	enum struct AccessModifier : uint8_t
+	ENUM(Access, uint8_t)
 	{//The base struct that handles this might be later named "ClassicInfos". Maybe. Will decide if it sounds dumb.
-
+		None,		//No access is given at this point.
 		Private,	//Only a given scope can observe it
 		Protected,	//Only the scope and derived scopes can observe it (derived data types or required/imported/included scripts). Means different things between data type and 
-		
+		//				script block usage.
 		//ProtectedInternal,//extends internal to derived scripts. In a class functions much like internal.
 		Public,
 
 		Internal = 1 << 7,	//Only the script can observe it. Is a flag rather than another thing
+		
+
+		NoneInternal = Access::Internal,
+		PrivateInternal = Access::Private | Access::Internal,
+		ProtectedInternal = Access::Protected | Access::Internal,
+		PublicInternal = Access::Public | Access::Internal,
+			
+			
 		//Protected		Only the scope and derived scopes can observe it (derived data types or required/imported/included scripts). Means different things between data type and 
-		//				script block usage.
+		
 	};
 }
