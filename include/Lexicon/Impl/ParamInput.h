c977f02786ce25251f90620fcdf9055692b96894
NoahBoddie <101380447+NoahBoddie@users.noreply.github.com>
2024-06-14 00:27:55 -0400

    Moved Filter_ functions to Qualifier
    Note, these will be moved again. They don't belong here either.

--- include/Lexicon/Impl/ParamInput.h
+++ include/Lexicon/Impl/ParamInput.h
@@ -12,37 +12,7 @@ namespace LEX
 	struct LocalInfo;
 
 
-	//Please relocate
-	template <auto T>
-	using TypeOf = decltype(T);
-
-
-	template <auto Filter> requires (std::is_enum_v<TypeOf<Filter>>)
-		constexpr bool FilterEquals(TypeOf<Filter> value, TypeOf<Filter> check)
-	{
-
-		using T = TypeOf<Filter>;
-
-		using U = std::underlying_type_t<T>;
-
-		U filtered = std::to_underlying(value) & std::to_underlying(Filter);
-
-		return filtered == std::to_underlying(check);
-	}
-
-	template <auto Filter> requires (std::is_enum_v<TypeOf<Filter>>)
-		constexpr TypeOf<Filter> FilterOR(TypeOf<Filter> value, TypeOf<Filter> add)
-	{
-
-		using T = TypeOf<Filter>;
-
-		using U = std::underlying_type_t<T>;
-		//We remove the filter from the value, and remove everything but the filter from add.
-		U fil_ret = std::to_underlying(value) & ~std::to_underlying(Filter);
-		U fil_add = std::to_underlying(add) & std::to_underlying(Filter);
-
-		return static_cast<T>(fil_ret | fil_add);
-	}
+	
 
 
 	struct ParamInput
