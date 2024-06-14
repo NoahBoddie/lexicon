f0654c7ce02cfa67ceec1a2f8d5f31ae9e3e0b9c
NoahBoddie <101380447+NoahBoddie@users.noreply.github.com>
2024-06-14 00:09:24 -0400

    Removed InterfaceManagerImpl
    -No longer need InterfaceManagerImpl, moved into InterfaceManager.cpp
    -Moved interface list to a function so it always loads in time

--- include/Lexicon/Impl/InterfaceManager.cpp
+++ include/Lexicon/Impl/InterfaceManager.cpp
@@ -1,10 +1,76 @@
 #pragma once
 
-#include "Interface.h"
 #include "InterfaceManager.h"
 
 
 namespace LEX
 {
+	std::unordered_map<std::string_view, Interface*>& GetInterfaceList()
+	{
+		//I handle this through due to the fact that register interface is handled along with the rest of the construction
+		// objects (honestly a dumb idea). This ensures no matter how early a singleton is constructed, the interface list is
+		// constructed before something tries to add to it.
+		static std::unordered_map<std::string_view, Interface*> interfaceList{};
+		
+		return interfaceList;
+	}
+	
+
+	bool RegisterInterface_Impl(Interface& ifc, std::string_view name)
+	{
+		auto& interfaceList = GetInterfaceList();
+		logger::info("registering. . .");
+		interfaceList[name] = &ifc;
+
+		logger::info("Registered interface");
+		return true;
+	}
+
+	//This should be an external function.
+	Update RequestInterface_Impl(Interface*& out, std::string_view name, uintptr_t version)
+	{
+		auto& interfaceList = GetInterfaceList();
+
+		auto it = interfaceList.find(name);
+
+		if (interfaceList.end() == it)
+			return Update::Invalid;
+
+		Interface* result = it->second;
+
+		//If the query is larger than what we can give, engine is out of date.
+		if (result->Version() < version)
+			return Update::Engine;
+
+		//If what we can give cannot handle the version queried
+		if (result->CanHandle(version) == false)
+			return Update::Library;
+
+		out = result;
+
+		return Update::Match;
+	}
+
+
+	bool InterfaceManager::RegisterInterface(Interface& ifc, std::string_view name)
+	{
+#ifdef LEX_SOURCE
+		//return RegisterInterface_Impl(ifc, name);
+#endif
+
+		//If this is the source, it should just directly use the implemetation.
+		using _Self = bool(Interface&, std::string_view);
+		return ExternCall<_Self>(TEMP__EXPECTED_MODULE(), "RegisterInterface_Impl", ifc, name);
+	}
+
+	Update InterfaceManager::RequestInterface(Interface*& out, std::string_view name, uintptr_t version)
+	{
+#ifdef LEX_SOURCE
+		//return RequestInterface_Impl(out, name, version);
+#endif
+
+		using _Self = Update(Interface*&, std::string_view, uintptr_t);
+		return ExternCall<_Self>(TEMP__EXPECTED_MODULE(), "RequestInterface_Impl", out, name, version);
+	}
 
 }
--- include/Lexicon/Impl/InterfaceManager.h
+++ include/Lexicon/Impl/InterfaceManager.h
@@ -1,45 +1,28 @@
 #pragma once
 
-
 #include "Interface.h"
-
-#ifdef LEX_SOURCE
-#include "InterfaceManagerImpl.h"
-#endif
 
 namespace LEX
 {
 	struct Interface;
 	
 	
+	extern "C" __declspec(dllexport) bool RegisterInterface_Impl(Interface& ifc, std::string_view name);
+
+	//This should be an external function.
+	extern "C" __declspec(dllexport) Update RequestInterface_Impl(Interface*& out, std::string_view name, uintptr_t version);
+
 	struct InterfaceManager
 	{
 #define TEMP__EXPECTED_MODULE() "Lexicon_Test.exe"
 
 
 
-		static bool RegisterInterface(Interface& ifc, std::string_view name)
-		{
-#ifdef LEX_SOURCE
-			//return RegisterInterface_Impl(ifc, name);
-#endif
-			
-			//If this is the source, it should just directly use the implemetation.
-			using _Self = bool(Interface&, std::string_view);
-			return ExternCall<_Self>(TEMP__EXPECTED_MODULE(), "RegisterInterface_Impl", ifc, name);
-		}
+		static bool RegisterInterface(Interface& ifc, std::string_view name);
 
 
 
-		static Update RequestInterface(Interface*& out, std::string_view name, uintptr_t version)
-		{
-#ifdef LEX_SOURCE
-			//return RequestInterface_Impl(out, name, version);
-#endif
-
-			using _Self = Update(Interface*&, std::string_view, uintptr_t);
-			return ExternCall<_Self>(TEMP__EXPECTED_MODULE(), "RequestInterface_Impl", out, name, version);
-		}
+		static Update RequestInterface(Interface*& out, std::string_view name, uintptr_t version);
 
 //#undef TEMP__EXPECTED_MODULE
 
