c6c389ec6b116635263fe289ffc1168b9c9a45ec
NoahBoddie <101380447+NoahBoddie@users.noreply.github.com>
2024-06-21 21:54:13 -0400

    Created new struct for function data
    Created basic callable data. To be used by formulas

--- include/Lexicon/Impl/FunctionData.h
+++ include/Lexicon/Impl/FunctionData.h
@@ -23,9 +23,127 @@ namespace LEX
 	//Or maybe it should be an enum.
 	using Procedure = void(*)(RuntimeVariable&, Variable* target, std::vector<Variable*>, ProcedureData&);
 	
+	struct BasicCallableData
+	{
+		QualifiedType _returnType = nullptr;
+
+		//Without a target type, this is a static function.
+		//Target type remains implicit
+		ITypePolicy* _targetType = nullptr;
+
+		//The actual object for this has severely changed. It's "ParamInfo" I think? But this isn't terribly important
+		// here yet so you know.
+		//std::vector<DeclaredParam> reqParams;
+		//std::vector<DefinedParam> defParams;
+
+		//TODO: I would like to represent "this" as a unique pointer to a ParamInfo
+		std::unique_ptr<ParameterInfo> __thisInfo;
+		std::vector<ParameterInfo> parameters;
 
 
-	struct FunctionData
+		RoutineBase _routine;  //actually needs to be a pointer
+
+
+		
+		RoutineBase* GetRoutine()
+		{
+			//This plans to be a pointer later, as this will end up just being
+			return &_routine;
+		}
+
+		//I won't care about any of these I'll keep it a buck.
+	protected:
+		auto _ParamBegin()
+		{
+			auto begin = parameters.begin();
+
+			return begin;
+		}
+
+		auto _ParamEnd()
+		{
+			auto end = parameters.end();
+			return end;
+		}
+
+		auto _ParamBegin() const
+		{
+			auto begin = parameters.begin();
+
+			return begin;
+		}
+
+		auto _ParamEnd() const
+		{
+			auto end = parameters.end();
+			return end;
+		}
+
+	public:
+		std::vector<ParameterInfo> GetParameters()
+		{
+			return { _ParamBegin(), _ParamEnd() };
+		}
+
+		std::vector<ParameterInfo> GetParameters() const
+		{
+			return { _ParamBegin(), _ParamEnd() };
+		}
+
+		size_t GetParamCount() const
+		{
+			return parameters.size();
+		}
+
+		size_t GetParamAllocSize() const
+		{
+			return parameters.size();
+		}
+
+		//For now, the maximum and minimum is both the same. Later, defaults will exist, so they don't have to
+		//be exact, and params keyword will hopefully exist at some point.
+		size_t GetReqArgCount()
+		{
+			return GetParamCount();
+		}
+		size_t GetMaxArgCount()
+		{
+			return GetParamCount();
+		}
+
+		std::array<size_t, 2> GetArgRange()
+		{
+			return { GetReqArgCount(), GetMaxArgCount() };
+		}
+
+		//Needs to be moved into ITypePolicy/AbstractTypePolicy.
+		QualifiedType GetReturnType() const
+		{
+			return _returnType;
+		}
+
+		ITypePolicy* GetTargetType() const
+		{
+			return _targetType;
+		}
+
+		//These sorts of things should be protected, not used up front.
+		//std::string name(){return _name;}
+
+		ParameterInfo* FindParameter(std::string a_name)
+		{
+			auto end = parameters.end();
+			auto it = std::find_if(parameters.begin(), end, [&](ParameterInfo& q) { return q.GetFieldName() == a_name; });
+			if (it != end) {
+				return &*it;
+			}
+			return nullptr;
+		}
+
+
+	};
+
+	struct FunctionData : public BasicCallableData
 	{
 		//A struct to be owned protected.
 
@@ -42,31 +160,12 @@ namespace LEX
 		String _name;
 
 		
+		size_t defaultIndex = (size_t)-1;  //max_value<size_t>;//basically whenever the defaults start.
 		
-		QualifiedType _returnType = nullptr;
-	
-
-		
-		//Without a target type, this is a static function.
-		//Target type remains implicit
-		ITypePolicy* _targetType = nullptr;
-
-
-
-		//The actual object for this has severely changed. It's "ParamInfo" I think? But this isn't terribly important
-		// here yet so you know.
-		//std::vector<DeclaredParam> reqParams;
-		//std::vector<DefinedParam> defParams;
-		
-		//TODO: I would like to represent "this" as a unique pointer to a ParamInfo
-		std::unique_ptr<ParameterInfo> __thisInfo;
-		std::vector<ParameterInfo> parameters;
-
-		size_t defaultIndex = (size_t)-1;//max_value<size_t>;//basically whenever the defaults start.
-
-		RoutineBase _routine;//actually needs to be a pointer
-
+		//
 		Procedure _procedure = nullptr;
+
+		//formulas won't have defaults, they don't have names, and they don't have procedures (such would defy the point of them.
 
 
 		RoutineBase* GetRoutine()
