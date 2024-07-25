3776ea6850af02b81ff57f75c73ed0323becf7c8
NoahBoddie <101380447+NoahBoddie@users.noreply.github.com>
2024-07-25 08:14:39 -0400

    Added common type
    common_type helps manage common types that will need to get referred to often enough to save the constant call.

---
+++ include/Lexicon/Impl/common_type.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "Lexicon/Number.h"
+#include "Lexicon/Interfaces/IdentityManager.h"
+
+namespace LEX
+{
+	struct common_type
+	{
+		//Common type is a class that keeps track of all the common types that may get used. Namely, boolean.
+
+
+
+		static ITypePolicy* boolean()
+		{
+			constexpr auto bool_settings = Number::Settings::CreateFromType<bool>();
+
+			static ITypePolicy* type = nullptr;
+
+			if (!type){
+				type = IdentityManager::instance->GetTypeByOffset("NUMBER", bool_settings.GetOffset());
+			}
+
+			return type;
+		}
+
+
+		static ITypePolicy* integer64()
+		{
+			constexpr auto int_settings = Number::Settings::CreateFromType<int32_t>();
+
+			static ITypePolicy* type = nullptr;
+
+			if (!type) {
+				type = IdentityManager::instance->GetTypeByOffset("NUMBER", int_settings.GetOffset());
+			}
+
+			return type;
+		}
+
+
+
+	private:
+		
+	};
+}
