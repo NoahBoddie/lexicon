11300becbde9cd6d56b93509ae14be4c3ccc1f0a
NoahBoddie <101380447+NoahBoddie@users.noreply.github.com>
2024-10-05 12:07:38 -0400

    Added Voidable InherentType

--- include/Lexicon/InherentType.h
+++ include/Lexicon/InherentType.h
@@ -4,14 +4,19 @@ namespace LEX
 {
 	//This is a list of build in types that exist before any other type comes into existence.
 	// This includes types like void, object, trival, or trival tuple.
-
+	
 	//A different name could be ReservedTypes.
 	ENUM(InherentType, uint8_t)
 	{
 		kVoid,
-		//kObject,//No Object, it may have a name as well as other elements.
+		kVoidable,//An interface all objects can be a part of, and the only thing void derives from.
+		//kObject,//No Object, it may have a name as well as other elements. (Then I could just name it.
 		//kTrival,
 		//kTuple,
+		_1,
+			_4,
+			_2,
+			_3,
 		kTotal,
 	};
 }
