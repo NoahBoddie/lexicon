cdb51da4af8b7aba1904f468db5d11c490f58c56
NoahBoddie <101380447+NoahBoddie@users.noreply.github.com>
2026-03-29 14:37:28 -0400

    Removed interface pivot files

--- include/Lexicon/IFunction.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-//A pivot file for ITypeInfo. Removes the hassle of knowing of which version to include.
-
-#ifdef LEX_SOURCE
-#include "Lexicon/Engine/IFunctionImpl.h"
-#else
-#include "Lexicon/IFunction.h"
-#endif
