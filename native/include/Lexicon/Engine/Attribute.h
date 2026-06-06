62177182a13295fcad496bc8c84996ce56ae71fc
NoahBoddie <101380447+NoahBoddie@users.noreply.github.com>
2026-06-06 02:37:29 -0400

    Attribute is no longer a Component
    -I realized it really doesn't need many stages of linking. All it needs is when it starts.

--- native/include/Lexicon/Engine/Attribute.h
+++ native/include/Lexicon/Engine/Attribute.h
@@ -11,7 +11,7 @@ namespace LEX
     //TODO: Instead of making this a component I'll use link complete in order to handle attribute addition
     // Attributes themselves will still have a function they use to load themselves (namely for the purpose)
     // of calling upon their constructors.
-    struct Attribute : public Component, public AttributeBase
+    struct Attribute : public AttributeBase
     {
         //Base object of both custom and native attribute
         IComponent* GetCompParent() override final
@@ -24,33 +24,13 @@ namespace LEX
             return dynamic_cast<Info*>(_parent);
         }
 
-        LinkFlag GetLinkFlags() override 
-        {
-            if (_parent) {
-                return _parent->GetLinkFlags();
-            }
+        std::string_view GetName() const;
 
-            return LinkFlag::None;
+        virtual void LoadAttribute(SyntaxRecord& record)
+        {
+
         }
 
-
-        std::string_view GetName() const override;
-
-        const Component* GetComponent() const override
-        {
-            return this;
-        }
-
-        const IComponent* GetComponentBase() const override
-        {
-            return nullptr;
-        }
-        
-        //I'll use link complete in order to handle attributes
-        void OnLinkComplete() override
-        {
-            OnTargetValidated();
-        }
         //I'm thinking these can only go on components.
         Component* _parent = nullptr;
     };
