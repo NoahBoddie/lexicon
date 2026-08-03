a1596460860f229dc43d653705d3ca571242af3d
NoahBoddie <101380447+NoahBoddie@users.noreply.github.com>
2026-08-03 12:32:00 -0400

    New SSO features, formatter, memory leak fix
    -LEX::String now handles small string optimizations. String counts below or equal to 15 (null char inclusive) will be stored within the struct itself
    -Added a feature where small strings can be created in constexpr settings with string literals.
    -Added a fmt formatter so it can be used straight within logs
    -Fixed a possible memory leak where allocated data wasn't released

--- include/Lexicon/String.h
+++ include/Lexicon/String.h
@@ -1,10 +1,15 @@
 #pragma once
 
+
+#include <string_view>
+
 #include "Lexicon/VariableType.h"
 
 //*src
 #include "Lexicon/TypeInfo.h"
 #include "Lexicon/Interfaces/IdentityManager.h"
+
+
 
 namespace LEX
 {
@@ -12,219 +17,459 @@ namespace LEX
 
 	using c_string = const char*;
 
-	struct String
-	{
+    struct String
+    {
+    private:
+        struct SmallData
+        {
+            char    chars[15]{};
+            int8_t  sizeByte{ 0 };
+        };
+        struct BigData
+        {
+            char* ptr = nullptr;
+            ptrdiff_t size = 0;
+        };
 
-		~String()
-		{
-			if (_ptr)
-				delete[] _ptr;
-		}
+        static constexpr uint8_t SMALL_STRING_LENGTH = sizeof(SmallData::chars);
 
-		constexpr String() noexcept = default;
+        constexpr static bool is_length_small(size_t size) noexcept
+        {
+            return SMALL_STRING_LENGTH >= size;
+        }
 
-		String(const char* str)
-		{
-			SetData(str, -1);
-		}
+    public:
 
-		String(std::string_view view)
-		{
-			SetData(view.data(), view.size());
-		}
+        constexpr ~String() { clear(); }
 
-		String(const std::string& str)
-		{
-			SetData(str.data(), str.size());
-		}
+        constexpr String() noexcept : _small{} {}
 
-		String(std::string&& str)
-		{
-			SetData(str.data(), str.size());
-		}
+        template<size_t N>
+        constexpr String(const char(&other)[N]) noexcept requires(N <= SMALL_STRING_LENGTH) {
+            set_data(other);
+        }
 
-
-		String(const String& other)
-		{
-			SetData(other._ptr, other._size);
-		}
-
-		explicit String(size_t length)
-		{
-			CreateData(length);
-		}
-
-		String(String&& other)
-		{
-			std::swap(_ptr, other._ptr);
-			std::swap(_size, other._size);
-		}
-
-
-		String& operator=(const char* str)
-		{
-			SetData(str, -1);
-			return *this;
-		}
-
-		String& operator=(std::string_view view)
-		{
-			SetData(view.data(), view.size());
-			return *this;
-		}
-
-		String& operator=(std::string str)
-		{
-			SetData(str.data(), str.size());
-			return *this;
-		}
-
-
-		String& operator=(const String& other)
-		{
-			SetData(other._ptr, other._size);
-			return *this;
-		}
-
-		String& operator=(String&& other)
-		{
-			_ptr = std::exchange(other._ptr, nullptr);
-			_size = std::exchange(other._size, 0);
-			return *this;
-		}
-
-		String operator+(const String& other)
-		{
-			if (size() == 0)
-				return other;
-			else if (other.size() == 0)
-				return *this;
-
-			//If either is 0, just return one or the other.
-			String buffer{ size() + other.size() };
-			//buffer._ptr
-			char* dest_ptr = buffer._ptr;
-
-			dest_ptr = std::strncpy(dest_ptr, _ptr, _size);
-			std::strncpy(dest_ptr, other._ptr, other._size);
-
-			return buffer;
-		}
-
-		String& operator+=(const String& other)
-		{
-			//I'd like to do this better
-			return *this = operator+(other);
-		}
+        template<size_t N>
+        String(const char(&other)[N]) noexcept requires(N >= SMALL_STRING_LENGTH) {
+            set_data(other);
+        }
 
 
 
-		std::string string() const
-		{
-			return { _ptr, _size };
-		}
+        //merge these into something like what string view does so it isn't shit with new strings.
+        String(const std::string_view& view){
+            set_data(view);
+        }
 
-		std::string_view view() const
-		{
-			return { _ptr, _size };
-		}
+        String(const std::string& str)
+        {
+            set_data(str);
+        }
 
+        template<typename Char>
+        String(Char other) noexcept requires(std::is_pointer_v<Char>&& std::is_same_v<std::remove_const_t<std::remove_pointer_t<Char>>, char>) {
+            set_data(other);
+        }
 
-		operator std::string() const
-		{
-			return string();
-		}
+        /*
 
-		operator std::string_view() const
-		{
-			return view();
-		}
+        String(const char* str)
+        {
+            set_data(str);
+        }
 
-	private:
+        
 
-		void Clear()
-		{
-			if (_ptr) {
-				delete[] _ptr;
-				_ptr = nullptr;
-				_size = 0;
-			}
-		}
+        //*/
 
-		
+        String(const String& other)
+        {
+            if (copy_small_string(other) == false) {
+                set_data(other.view());
+            }
+        }
 
-		void CreateData(size_t length)
-		{
-			Clear();
-			
-			_ptr = new char[length + 1];
-			_ptr[length] = '\0';
-			_size = length;
-		}
-
-		void SetData(const char* str, size_t length)
-		{
-			//If ownership becomes a worry, lexicon will be the thing that creates and manages these strings.
-
-			//This needs to add some, due to the null termination
-			if (length == -1)
-				length = std::strlen(str);
-
-			if (!str || !length) {
-				Clear();
-				return;
-			}
-
-			CreateData(length);
-			std::strncpy(_ptr, str, length);
-		}
+        String(String&& other)
+        {
+            if (move_small_string(other) == false) {
+                _big.ptr = std::exchange(other._big.ptr, nullptr);
+                _big.size = std::exchange(other._big.size, 0);
+            }
+        }
 
 
-		//I could get away with using a variant for this.
-		char* _ptr = nullptr;
-		size_t _size = 0;
-	public:
+        explicit String(size_t length)
+        {
+            create_data(length);
+            
+            auto data = ptr();
 
-		[[nodiscard]] inline const char* data() const noexcept { return _ptr; }
-		[[nodiscard]] inline const char* c_str() const noexcept { return data(); }
-		[[nodiscard]] inline size_t size() const noexcept { return _size; }
+            for (size_t i = 0; i < length; ++i) {
+                data[i] = '\n';
+            }
+        }
 
+        String& operator=(const char* str)
+        {
+            set_data(str);
+            return *this;
+        }
 
-		std::strong_ordering operator<=>(const String& other) const
-		{
-			auto test = std::strncmp(_ptr, other._ptr, _size > other._size ? _size : other._size);
+        String& operator=(std::string_view view)
+        {
+            set_data(view);
+            return *this;
+        }
 
-			if (test > 0)
-				return std::strong_ordering::greater;
-			if (test < 0)
-				return std::strong_ordering::less;
-			else
-				return std::strong_ordering::equal;
-
-		}
-
-		bool operator==(const String& other) const = default;
-
-
-		constexpr bool IsValueZero() const noexcept
-		{
-			return !_ptr && !_size;
-		}
-
-		static TypeInfo* GetVariableType(const String*)
-		{
-			ITypeInfo* policy = IdentityManager::instance->GetTypeByOffset("STRING", 0);
-
-			//Should already be specialized, so just sending it.
-			return NULL_OP(NULL_Q(policy)->GetTypeInfo(nullptr));
-		}
+        String& operator=(std::string str)
+        {
+            set_data(str);
+            return *this;
+        }
 
 
-	};
-	REQUIRED_SIZE(String, 0x10);
+        String& operator=(const String& other)
+        {
+            if (copy_small_string(other) == false) {
+                set_data(other.view());
+            }
+
+            return *this;
+        }
+
+        String& operator=(String&& other)
+        {
+            if (move_small_string(other) == false) {
+                clear();
+                _big.ptr = std::exchange(other._big.ptr, nullptr);
+                _big.size = std::exchange(other._big.size, 0);
+            }
+            return *this;
+        }
+
+        String operator+(const String& other)
+        {
+            auto lhs_size = size();
+            if (!lhs_size)
+                return other;
+
+            auto rhs_size = other.size();
+
+            if (!rhs_size)
+                return *this;
+
+            //If either is 0, just return one or the other.
+            String buffer{};
+            buffer.create_data(lhs_size + rhs_size);
+
+            //buffer._ptr
+            char* dest_ptr = buffer.ptr();
+
+            dest_ptr = std::strncpy(dest_ptr, ptr(), lhs_size);
+            std::strncpy(dest_ptr, other.ptr(), rhs_size);
+
+            return buffer;
+        }
+
+        String& operator+=(const String& other)
+        {
+            //I'd like to do this better
+            return *this = operator+(other);
+        }
 
 
 
+        std::string string() const
+        {
+            return { ptr(), size() };
+        }
+
+        std::string_view view() const
+        {
+            return { ptr(), size() };
+        }
+
+
+        operator std::string() const
+        {
+            return string();
+        }
+
+        operator std::string_view() const
+        {
+            return view();
+        }
+
+    private:
+
+
+        constexpr void clear() noexcept
+        {
+            if (!is_small() && _big.ptr)
+                delete[] _big.ptr;
+
+            _small = SmallData{};
+        }
+
+        bool copy_small_string(const String& other)
+        {
+            bool result = other.is_small();
+
+            if (result) {
+                std::memcpy(this, &other, sizeof(String));
+            }
+
+            return result;
+        }
+
+        bool move_small_string(String& other)
+        {
+            bool result = copy_small_string(other);
+
+            if (result) {
+                other.clear();
+            }
+
+            return result;
+        }
+
+        bool move_small_string(String&& other)
+        {
+            move_small_string(other);
+        }
+
+
+        constexpr void create_data(size_t length) noexcept
+        {
+            clear();
+
+            if (std::is_constant_evaluated() || is_length_small(length) == true) {
+                _small.sizeByte = sign_value<int8_t> | static_cast<int8_t>(length);
+            }
+            else {
+                _big.ptr = new char[length + 1];
+                _big.ptr[length] = '\0';
+                _big.size = length;
+            }
+        }
+
+
+        constexpr void set_data(const std::string_view& str) noexcept
+        {
+            //If ownership becomes a worry, lexicon will be the thing that creates and manages these strings.
+
+            if (str.empty() == true) {
+                clear();
+                return;
+            }
+
+            size_t length = str.size();
+
+            create_data(length);
+
+
+            std::char_traits<char>::copy(ptr(), str.data(), length);
+            //std::strncpy(ptr(), str.data(), length);
+        }
+
+
+        [[nodiscard]] constexpr const char* ptr() const noexcept {
+            return unconst(this)->ptr();
+        }
+
+
+        [[nodiscard]] constexpr char* ptr() noexcept {
+            if (is_small() == true)
+                return _small.chars;
+            else
+                return _big.ptr;
+        }
+
+
+
+
+        constexpr bool is_small() const noexcept
+        {
+            //The only way a lex string exist at const eval is if it's small.
+            if (std::is_constant_evaluated() == false) {
+                //If it's not constant evaluated, regardless if it's big or small, this checks for both being zero,
+                // as well as both being negative.
+                return _big.size <= 0;
+            }
+
+            return true;
+        }
+
+
+
+
+
+
+        //I could get away with using a variant for this.
+
+
+    public:
+
+        [[nodiscard]] inline const char* data() const noexcept { return ptr(); }
+        [[nodiscard]] inline const char* c_str() const noexcept { return data(); }
+        constexpr size_t size() const noexcept {
+            if (is_small() == true) {
+                return _small.sizeByte & ~sign_value<int8_t>;
+            }
+            else {
+                return _big.size;
+            }
+        }
+
+        constexpr bool empty() const noexcept
+        {
+            return !size();
+        }
+
+        
+        constexpr std::strong_ordering operator<=>(const String& other) const noexcept
+        {
+            auto len = size();
+
+            if (auto size = other.size(); len != size) {
+                if (len < size)
+                    return std::strong_ordering::less;
+                else
+                    return std::strong_ordering::greater;
+            }
+            else if (len) {
+                int test = std::char_traits<char>::compare(ptr(), other.ptr(), len + 1);
+
+                if (test > 0)
+                    return std::strong_ordering::greater;
+                
+                if (test < 0)
+                    return std::strong_ordering::less;
+            }
+
+            return std::strong_ordering::equal;
+
+
+            //return {};
+            //*
+            if (std::is_constant_evaluated()) {
+                const char* lhs = ptr();
+                const char* rhs = other.ptr();
+
+                std::strcmp(lhs, rhs);
+
+                auto length = std::min(size(), other.size());
+                for (size_t i = 0; i < length; ++i) {
+                    if (auto result = lhs[i] <=> rhs[i]; result != std::strong_ordering::equal)
+                        return result;
+                }
+
+                return std::strong_ordering::equal;
+            }
+            else {
+                int test = std::strncmp(ptr(), other.ptr(), std::min(size(), other.size()));
+
+                if (test > 0)
+                    return std::strong_ordering::greater;
+                if (test < 0)
+                    return std::strong_ordering::less;
+                else
+                    return std::strong_ordering::equal;
+
+            }
+            //*/
+        }
+
+        constexpr bool operator==(const String& other) const
+        {
+            return operator<=>(other) == std::strong_ordering::equal;
+        }
+
+        
+
+
+    public:
+
+        constexpr bool IsValueZero() const noexcept
+        {
+            return empty();
+        }
+
+        static TypeInfo* GetVariableType(const String*)
+        {
+            ITypeInfo* policy = IdentityManager::instance->GetTypeByOffset("STRING", 0);
+
+            //Should already be specialized, so just sending it.
+            return NULL_OP(NULL_Q(policy)->GetTypeInfo(nullptr));
+        }
+    private:
+        /*
+
+        union
+        {
+            char* _ptr = nullptr;
+            char    _chars[1];//...
+
+        };
+        union
+        {
+            struct
+            {
+                uint8_t l_charBytes[7];
+                int8_t l_sizeByte;
+            };
+
+            //struct
+            //{
+            //    int8_t lil_controlByte;
+            //    uint8_t lil_testBytes[7];
+            //};
+
+            intptr_t _size = 0;
+        };
+
+        //*/
+
+        /*
+
+        union
+        {
+            char* _ptr = nullptr;
+            char    _chars[1];//...
+
+        };
+        union
+        {
+            struct
+            {
+                uint8_t l_charBytes[7];
+                int8_t l_sizeByte;
+            };
+
+            //struct
+            //{
+            //    int8_t lil_controlByte;
+            //    uint8_t lil_testBytes[7];
+            //};
+
+            intptr_t _size = 0;
+        };
+
+        //*/
+
+        union
+        {
+            
+            SmallData   _small{};
+            BigData     _big;
+        };
+
+        //*/
+    };
+    REQUIRED_SIZE(String, 0x10);
+
+
+    //Make this any kind of string type.
 	template <>
 	struct VariableType<std::string>
 	{
@@ -245,12 +490,27 @@ namespace LEX
 		}
 	};
 
-	inline void Testing()
-	{
-		String test;
 
-		auto cast = static_cast<std::string>(test);
-	}
 
 
 }
+
+#ifndef __INTELLISENSE__
+//Turning off intellisense on this because it's annoying as shit.
+template <>
+struct fmt::formatter<LEX::String>
+{
+    template <class ParseContext>
+    constexpr auto parse(ParseContext& a_ctx)
+    {
+        return a_ctx.begin();
+    }
+
+
+    // Forwards the formatting by casting the enum to it's underlying type
+    auto format(const LEX::String& str, fmt::format_context& ctx) const
+    {
+        return fmt::format_to(ctx.out(), "{}", str.view());;
+    }
+};
+#endif
